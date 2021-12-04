d63810051b0788d73f0444b2c0b2e2cfd26e9b32
KalTang <ktang82@my.bcit.ca>
2021-12-04 00:53:28 -0800

    test country api data

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -11,18 +11,23 @@ import CovidAPI from '../api/CovidAPI';
 // import { Avatar, Card, Title, Paragraph } from 'react-native-paper';
 
 const CountryCard = () => {
+    // Hooks
+    const [country, setCountry] = useState('');
+
     useEffect(() => {
         countryResponse();
     }, []);
 
+    // Fetches data from Api
     const countryResponse = async () => {
         const response = await CovidAPI.get('summary');
-        console.log(response.data.Global);
+        console.log(response.data.Countries[1].Country);
+        setCountry(response.data.Countries[1].Country);
     };
     return (
         <TouchableOpacity>
             <View style={styles.container}>
-                <Text style={styles.countryText}>Canada</Text>
+                <Text style={styles.countryText}>{country}</Text>
             </View>
         </TouchableOpacity>
     );
